1fa4c458dd9efb9322591cc812c46cdcab4bb16b
Erwin Elder <erwineldermail@gmail.com>
2024-04-07 19:31:58 +0200

    Update to version 0.1.4.

--- app/src/main/java/com/ataglance/walletglance/ui/theme/screens/settings/SettingsHomeScreen.kt
+++ app/src/main/java/com/ataglance/walletglance/ui/theme/screens/settings/SettingsHomeScreen.kt
@@ -71,7 +71,7 @@ fun SettingsHomeScreen(
                 fontFamily = Manrope
             )
             Text(
-                text = stringResource(R.string.version) + " 0.1.3",
+                text = stringResource(R.string.version) + " 0.1.4",
                 color = GlanceTheme.onSurface,
                 fontSize = 16.sp,
                 letterSpacing = 0.sp,
