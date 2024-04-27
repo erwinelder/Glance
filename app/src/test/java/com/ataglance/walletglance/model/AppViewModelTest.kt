0fe3c45003b14529c5dd67307c3543e0809ddfb2
Erwin Elder <erwineldermail@gmail.com>
2024-04-27 22:02:44 +0200

    Add tests for categories order numbers fix.

--- app/src/test/java/com/ataglance/walletglance/model/AppViewModelTest.kt
+++ app/src/test/java/com/ataglance/walletglance/model/AppViewModelTest.kt
@@ -2,12 +2,16 @@ package com.ataglance.walletglance.model
 
 import com.ataglance.walletglance.data.Account
 import com.ataglance.walletglance.data.AccountRepository
+import com.ataglance.walletglance.data.Category
 import com.ataglance.walletglance.data.CategoryRepository
 import com.ataglance.walletglance.data.GeneralRepository
 import com.ataglance.walletglance.data.RecordAndAccountRepository
 import com.ataglance.walletglance.data.RecordRepository
 import com.ataglance.walletglance.data.SettingsRepository
+import org.junit.Assert
+import org.junit.Assert.assertArrayEquals
 import org.junit.Assert.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -192,7 +196,187 @@ class AppViewModelTest {
             uiState, recordStacks.first, recordStacks.second
         )
 
-        assertEquals(getAccountListByBalances(expectedBalances), result)
+        assertEquals(getAccountListByBalances(expectedBalances), result?.sortedBy { it.id })
+    }
+
+
+
+    @Test
+    fun testBreakCategoriesOnDifferentLists() {
+        val categoryList = listOf(
+            Category(
+                id = 1, type = '-', rank = 'c', orderNum = 1, parentCategoryId = 1,
+                name = "category 1", iconName = "",
+                colorName = CategoryColors.Olive(null).color.name
+            ),
+            Category(
+                id = 2, type = '-', rank = 'c', orderNum = 2, parentCategoryId = 2,
+                name = "category 2", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+
+            Category(
+                id = 13, type = '-', rank = 's', orderNum = 1, parentCategoryId = 1,
+                name = "subcategory 11", iconName = "",
+                colorName = CategoryColors.Olive(null).color.name
+            ),
+            Category(
+                id = 14, type = '-', rank = 's', orderNum = 2, parentCategoryId = 1,
+                name = "subcategory 12", iconName = "",
+                colorName = CategoryColors.Olive(null).color.name
+            ),
+
+            Category(
+                id = 15, type = '-', rank = 's', orderNum = 1, parentCategoryId = 2,
+                name = "subcategory 21", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 16, type = '-', rank = 's', orderNum = 1, parentCategoryId = 2,
+                name = "subcategory 22", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 17, type = '-', rank = 's', orderNum = 3, parentCategoryId = 2,
+                name = "subcategory 23", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 18, type = '-', rank = 's', orderNum = 4, parentCategoryId = 2,
+                name = "subcategory 24", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 19, type = '-', rank = 's', orderNum = 5, parentCategoryId = 2,
+                name = "subcategory 25", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 20, type = '-', rank = 's', orderNum = 6, parentCategoryId = 2,
+                name = "subcategory 26", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            )
+        )
+
+        Assert.assertThrows(IllegalAccessException::class.java) {
+            CategoryController().breakCategoriesOnDifferentLists(categoryList)
+        }
+    }
+
+    @Test
+    fun testFixCategoriesOrderNums() {
+        val currentCategoryList = listOf(
+            Category(
+                id = 1, type = '-', rank = 'c', orderNum = 1, parentCategoryId = 1,
+                name = "category 1", iconName = "",
+                colorName = CategoryColors.Olive(null).color.name
+            ),
+            Category(
+                id = 2, type = '-', rank = 'c', orderNum = 1, parentCategoryId = 2,
+                name = "category 2", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+
+            Category(
+                id = 13, type = '-', rank = 's', orderNum = 1, parentCategoryId = 1,
+                name = "subcategory 11", iconName = "",
+                colorName = CategoryColors.Olive(null).color.name
+            ),
+            Category(
+                id = 14, type = '-', rank = 's', orderNum = 2, parentCategoryId = 1,
+                name = "subcategory 12", iconName = "",
+                colorName = CategoryColors.Olive(null).color.name
+            ),
+
+            Category(
+                id = 15, type = '-', rank = 's', orderNum = 1, parentCategoryId = 2,
+                name = "subcategory 21", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 16, type = '-', rank = 's', orderNum = 1, parentCategoryId = 2,
+                name = "subcategory 22", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 17, type = '-', rank = 's', orderNum = 3, parentCategoryId = 2,
+                name = "subcategory 23", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 18, type = '-', rank = 's', orderNum = 4, parentCategoryId = 2,
+                name = "subcategory 24", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 19, type = '-', rank = 's', orderNum = 5, parentCategoryId = 2,
+                name = "subcategory 25", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 20, type = '-', rank = 's', orderNum = 6, parentCategoryId = 2,
+                name = "subcategory 26", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            )
+        )
+        val expectedCategoryList = listOf(
+            Category(
+                id = 1, type = '-', rank = 'c', orderNum = 1, parentCategoryId = 1,
+                name = "category 1", iconName = "",
+                colorName = CategoryColors.Olive(null).color.name
+            ),
+            Category(
+                id = 2, type = '-', rank = 'c', orderNum = 2, parentCategoryId = 2,
+                name = "category 2", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+
+            Category(
+                id = 13, type = '-', rank = 's', orderNum = 1, parentCategoryId = 1,
+                name = "subcategory 11", iconName = "",
+                colorName = CategoryColors.Olive(null).color.name
+            ),
+            Category(
+                id = 14, type = '-', rank = 's', orderNum = 2, parentCategoryId = 1,
+                name = "subcategory 12", iconName = "",
+                colorName = CategoryColors.Olive(null).color.name
+            ),
+
+            Category(
+                id = 15, type = '-', rank = 's', orderNum = 1, parentCategoryId = 2,
+                name = "subcategory 21", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 16, type = '-', rank = 's', orderNum = 2, parentCategoryId = 2,
+                name = "subcategory 22", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 17, type = '-', rank = 's', orderNum = 3, parentCategoryId = 2,
+                name = "subcategory 23", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 18, type = '-', rank = 's', orderNum = 4, parentCategoryId = 2,
+                name = "subcategory 24", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 19, type = '-', rank = 's', orderNum = 5, parentCategoryId = 2,
+                name = "subcategory 25", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            ),
+            Category(
+                id = 20, type = '-', rank = 's', orderNum = 6, parentCategoryId = 2,
+                name = "subcategory 26", iconName = "",
+                colorName = CategoryColors.Camel(null).color.name
+            )
+        )
+        assertArrayEquals(
+            expectedCategoryList.toTypedArray(),
+            CategoryController().fixCategoriesOrderNums(currentCategoryList).toTypedArray()
+        )
     }
 
 }
