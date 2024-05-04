5ed1d9779fe5a0f0bfdb048ae19e8633f60dd3c0
Erwin Elder <erwineldermail@gmail.com>
2024-05-04 13:41:12 +0200

    Update to version 1.0.10.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ataglance.walletglance"
         minSdk = 28
         targetSdk = 34
-        versionCode = 36
-        versionName = "1.0.9"
+        versionCode = 37
+        versionName = "1.0.10"
         resourceConfigurations += arrayOf("en", "cs", "de", "ru", "uk")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
