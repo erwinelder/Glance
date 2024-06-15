4ce8645d9dc43661dfc2210c333027c42f01696a
Erwin Elder <erwineldermail@gmail.com>
2024-06-15 09:08:50 +0200

    Remove redundant BoxScope variant.

--- app/src/main/java/com/ataglance/walletglance/ui/theme/uielements/buttons/SmallFilledIconButton.kt
+++ app/src/main/java/com/ataglance/walletglance/ui/theme/uielements/buttons/SmallFilledIconButton.kt
@@ -1,16 +1,15 @@
 package com.ataglance.walletglance.ui.theme.uielements.buttons
 
-import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.ataglance.walletglance.ui.theme.GlanceTheme
 import com.ataglance.walletglance.ui.theme.animation.bounceClickEffect
@@ -19,8 +18,9 @@ import com.ataglance.walletglance.ui.theme.animation.bounceClickEffect
 fun SmallFilledIconButton(
     iconRes: Int,
     iconContendDescription: String,
-    containerColor: Color = GlanceTheme.glassGradientLightToDark.first,
+    containerColor: Color = GlanceTheme.surface,
     contentColor: Color = GlanceTheme.onSurface,
+    size: Dp = 24.dp,
     enabled: Boolean = true,
     onClick: () -> Unit = {}
 ) {
@@ -37,37 +37,7 @@ fun SmallFilledIconButton(
         Icon(
             painter = painterResource(iconRes),
             contentDescription = iconContendDescription,
-            modifier = Modifier.size(24.dp)
-        )
-    }
-}
-
-@Composable
-fun BoxScope.SmallFilledIconButton(
-    iconRes: Int,
-    iconContendDescription: String,
-    containerColor: Color = GlanceTheme.surface,
-    contentColor: Color = GlanceTheme.onSurface,
-    alignment: Alignment = Alignment.Center,
-    enabled: Boolean = true,
-    onClick: () -> Unit = {}
-) {
-    FilledIconButton(
-        onClick = onClick,
-        enabled = enabled,
-        shape = RoundedCornerShape(13.dp),
-        colors = IconButtonDefaults.iconButtonColors(
-            containerColor = containerColor,
-            contentColor = contentColor
-        ),
-        modifier = Modifier
-            .bounceClickEffect(.96f)
-            .align(alignment)
-    ) {
-        Icon(
-            painter = painterResource(iconRes),
-            contentDescription = iconContendDescription,
-            modifier = Modifier.size(24.dp)
+            modifier = Modifier.size(size)
         )
     }
 }
