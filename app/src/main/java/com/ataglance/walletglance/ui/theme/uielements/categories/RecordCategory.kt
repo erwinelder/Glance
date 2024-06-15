9dfe4e116945b9f4878251b7220f912064f36f01
Erwin Elder <erwineldermail@gmail.com>
2024-06-15 09:14:43 +0200

    Reformat RecordCategory ui component.

--- app/src/main/java/com/ataglance/walletglance/ui/theme/uielements/categories/RecordCategory.kt
+++ app/src/main/java/com/ataglance/walletglance/ui/theme/uielements/categories/RecordCategory.kt
@@ -11,13 +11,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ataglance.walletglance.domain.entities.Category
 import com.ataglance.walletglance.ui.theme.GlanceTheme
 
 @Composable
-fun RecordCategory(categoryAndIconRes: Pair<Category?, Int?>?) {
+fun RecordCategory(
+    categoryAndIconRes: Pair<Category?, Int?>?,
+    iconSize: Dp = 22.dp,
+    fontSize: TextUnit = 18.sp
+) {
     categoryAndIconRes?.first?.let {
         Row(
             horizontalArrangement = Arrangement.spacedBy(8.dp),
@@ -28,13 +34,13 @@ fun RecordCategory(categoryAndIconRes: Pair<Category?, Int?>?) {
                     painter = painterResource(categoryAndIconRes.second!!),
                     contentDescription = categoryAndIconRes.first!!.name + " icon",
                     tint = GlanceTheme.onSurface,
-                    modifier = Modifier.size(22.dp)
+                    modifier = Modifier.size(iconSize)
                 )
             }
             Text(
                 text = categoryAndIconRes.first!!.name,
                 color = GlanceTheme.onSurface,
-                fontSize = 18.sp,
+                fontSize = fontSize,
                 fontWeight = FontWeight.Light,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
