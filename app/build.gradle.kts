69dfc699db61263c8da1f9f6658d88b6d85c61f9
Erwin Elder <erwineldermail@gmail.com>
2024-07-12 15:09:13 +0200

    Update dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,6 +98,7 @@ dependencies {
 
     // Room
     implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.junit.ktx)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
 
