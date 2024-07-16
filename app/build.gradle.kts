968738a234a4559ba68a60aab49eb4e750308b7c
Erwin Elder <erwineldermail@gmail.com>
2024-07-16 20:32:19 +0200

    Update to 47 version code.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.ataglance.walletglance"
         minSdk = 28
         targetSdk = 34
-        versionCode = 46
+        versionCode = 47
         versionName = "2.0"
         resourceConfigurations += arrayOf("en", "cs", "de", "ru", "es", "uk")
 
