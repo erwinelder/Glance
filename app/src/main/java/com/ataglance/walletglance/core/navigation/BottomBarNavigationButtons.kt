3a02e4c6913c73fc0f91b7e5a615d7895ba5db98
Erwin Elder <erwineldermail@gmail.com>
2024-08-26 19:16:34 +0200

    Add screen names.

--- app/src/main/java/com/ataglance/walletglance/core/navigation/BottomBarNavigationButtons.kt
+++ app/src/main/java/com/ataglance/walletglance/core/navigation/BottomBarNavigationButtons.kt
@@ -1,16 +1,19 @@
 package com.ataglance.walletglance.core.navigation
 
+import androidx.annotation.StringRes
 import com.ataglance.walletglance.R
 import com.ataglance.walletglance.core.domain.app.DrawableResByTheme
 
 sealed class BottomBarNavigationButtons(
     val screen: MainScreens,
+    @StringRes val screenNameRes: Int,
     val inactiveIconRes: DrawableResByTheme,
     val activeIconRes: DrawableResByTheme,
 ) {
 
     data object Home : BottomBarNavigationButtons(
         screen = MainScreens.Home,
+        screenNameRes = R.string.home,
         inactiveIconRes = DrawableResByTheme(
             lightDefault = R.drawable.home_light_inactive,
             darkDefault = R.drawable.home_dark_inactive
@@ -23,6 +26,7 @@ sealed class BottomBarNavigationButtons(
 
     data object Records : BottomBarNavigationButtons(
         screen = MainScreens.Records,
+        screenNameRes = R.string.records,
         inactiveIconRes = DrawableResByTheme(
             lightDefault = R.drawable.records_light_inactive,
             darkDefault = R.drawable.records_dark_inactive
@@ -35,6 +39,7 @@ sealed class BottomBarNavigationButtons(
 
     data object CategoryStatistics : BottomBarNavigationButtons(
         screen = MainScreens.CategoryStatistics(0),
+        screenNameRes = R.string.statistics,
         inactiveIconRes = DrawableResByTheme(
             lightDefault = R.drawable.statistics_light_inactive,
             darkDefault = R.drawable.statistics_dark_inactive
@@ -47,6 +52,7 @@ sealed class BottomBarNavigationButtons(
 
     data object Budgets : BottomBarNavigationButtons(
         screen = MainScreens.Budgets,
+        screenNameRes = R.string.budgets,
         inactiveIconRes = DrawableResByTheme(
             lightDefault = R.drawable.budgets_light_inactive,
             darkDefault = R.drawable.budgets_dark_inactive
@@ -59,6 +65,7 @@ sealed class BottomBarNavigationButtons(
 
     data object Settings : BottomBarNavigationButtons(
         screen = MainScreens.Settings,
+        screenNameRes = R.string.settings,
         inactiveIconRes = DrawableResByTheme(
             lightDefault = R.drawable.settings_light_inactive,
             darkDefault = R.drawable.settings_dark_inactive
