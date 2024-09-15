243bfc5fcdb02ed53821832645f145a0368c78a8
Erwin Elder <erwineldermail@gmail.com>
2024-09-15 19:11:59 +0200

    Add widget component with settings button.

--- app/src/main/java/com/ataglance/walletglance/core/presentation/components/widgets/WidgetWithTitleAndSettingsComponent.kt
+++ app/src/main/java/com/ataglance/walletglance/core/presentation/components/widgets/WidgetWithTitleAndSettingsComponent.kt
@@ -4,19 +4,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.ataglance.walletglance.R
 import com.ataglance.walletglance.core.domain.app.FilledWidthByScreenType
 
 @Composable
-fun WidgetWithTitleComponent(
+fun WidgetWithTitleAndSettingsComponent(
     filledWidthByScreenType: FilledWidthByScreenType = FilledWidthByScreenType(),
     contentPadding: PaddingValues = PaddingValues(16.dp),
     title: String,
+    onSettingsButtonClick: () -> Unit,
     content: @Composable ColumnScope.() -> Unit
 ) {
     WidgetComponent(filledWidthByScreenType = filledWidthByScreenType) {
@@ -27,7 +33,20 @@ fun WidgetWithTitleComponent(
                 .fillMaxWidth()
                 .padding(contentPadding)
         ) {
-            WidgetTitleComponent(title = title)
+            Row {
+                WidgetTitleComponent(
+                    title = title,
+                    modifier = Modifier.weight(1f, fill = false)
+                )
+                IconButton(
+                    onClick = onSettingsButtonClick
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.settings_icon),
+                        contentDescription = "widget settings",
+                    )
+                }
+            }
             content()
         }
     }
