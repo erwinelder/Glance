3ed8117c592917b60993aa2e2b0df62e85ca44e4
Erwin Elder <erwineldermail@gmail.com>
2024-09-20 20:18:14 +0200

    Visual improvements.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.ataglance.walletglance"
         minSdk = 28
         targetSdk = 34
-        versionCode = 57
+        versionCode = 58
         versionName = "3.0"
         resourceConfigurations += arrayOf("en", "cs", "de", "ru", "es", "uk")
 
--- app/src/main/java/com/ataglance/walletglance/budget/presentation/components/DefaultBudgetComponent.kt
+++ app/src/main/java/com/ataglance/walletglance/budget/presentation/components/DefaultBudgetComponent.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.unit.dp
 import com.ataglance.walletglance.R
 import com.ataglance.walletglance.budget.domain.model.Budget
 import com.ataglance.walletglance.budget.presentation.screen.EditBudgetsScreenPreview
+import com.ataglance.walletglance.core.presentation.GlanceTheme
 
 @Composable
 fun DefaultBudgetComponent(
@@ -24,6 +25,7 @@ fun DefaultBudgetComponent(
         Icon(
             painter = painterResource(R.drawable.short_arrow_right_icon),
             contentDescription = "right arrow icon",
+            tint = GlanceTheme.onSurface,
             modifier = Modifier.size(24.dp)
         )
     }
