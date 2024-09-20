8f8c1ba63c58adce390916049b3e55200e9501c2
Erwin Elder <erwineldermail@gmail.com>
2024-09-20 22:20:50 +0200

    Update app version code to 60.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.ataglance.walletglance"
         minSdk = 28
         targetSdk = 34
-        versionCode = 59
+        versionCode = 60
         versionName = "3.0"
         resourceConfigurations += arrayOf("en", "cs", "de", "ru", "es", "uk")
 
