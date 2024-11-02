0323c5fac598f7030dd05966e731a588e136672d
Erwin Elder <erwineldermail@gmail.com>
2024-11-02 14:15:52 +0100

    Refactor AuthViewModel.

--- app/src/main/java/com/ataglance/walletglance/auth/presentation/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/ataglance/walletglance/auth/presentation/viewmodel/AuthViewModel.kt
@@ -3,30 +3,26 @@ package com.ataglance.walletglance.auth.presentation.viewmodel
 import com.ataglance.walletglance.auth.domain.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.tasks.await
 
 class AuthViewModel(
     private val auth: FirebaseAuth
 ) {
 
     var user = User()
-    private val firebaseUser: FirebaseUser? = auth.currentUser
 
-    fun createNewUser(email: String, password: String) {
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                task.result.user?.let {
-                    user = User(uid = it.uid)
-                }
-            }
+    private fun setUser(user: FirebaseUser) {
+        this.user = User(uid = user.uid)
     }
 
-    fun signIn(email: String, password: String) {
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                task.result.user?.let {
-                    user = User(uid = it.uid)
-                }
-            }
+    suspend fun createNewUser(email: String, password: String) {
+        val result = auth.createUserWithEmailAndPassword(email, password).await()
+        result.user?.let(::setUser)
+    }
+
+    suspend fun signIn(email: String, password: String) {
+        val result = auth.signInWithEmailAndPassword(email, password).await()
+        result.user?.let(::setUser)
     }
 
     fun signOut() {
