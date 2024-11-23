f45816eb34635254336a9337ba1779c9062926f2
Erwin Elder <erwineldermail@gmail.com>
2024-11-23 14:11:31 +0100

    Improve IconWithBackground component appearance.

--- app/src/main/java/com/ataglance/walletglance/core/presentation/components/other/IconWithBackground.kt
+++ app/src/main/java/com/ataglance/walletglance/core/presentation/components/other/IconWithBackground.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -29,6 +30,11 @@ fun IconWithBackground(
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
+            .shadow(
+                elevation = 8.dp,
+                shape = RoundedCornerShape(50),
+                spotColor = backgroundGradient.first()
+            )
             .clip(RoundedCornerShape(50))
             .background(
                 brush = Brush.linearGradient(
