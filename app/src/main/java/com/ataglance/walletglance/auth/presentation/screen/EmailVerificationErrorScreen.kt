7ec01946430d857d6707bfa9a0e2516338f6d66b
Erwin Elder <erwineldermail@gmail.com>
2024-11-25 19:36:51 +0100

    Fix EmailVerificationErrorScreen.

--- app/src/main/java/com/ataglance/walletglance/auth/presentation/screen/EmailVerificationErrorScreen.kt
+++ app/src/main/java/com/ataglance/walletglance/auth/presentation/screen/EmailVerificationErrorScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.ataglance.walletglance.R
 import com.ataglance.walletglance.core.domain.app.AppTheme
+import com.ataglance.walletglance.core.presentation.GlanceTheme
 import com.ataglance.walletglance.core.presentation.components.screenContainers.PreviewWithMainScaffoldContainer
 import com.ataglance.walletglance.errorHandling.presentation.components.screenContainers.ResultMessageScreenContainer
 
@@ -15,6 +16,9 @@ fun EmailVerificationErrorScreen(
 ) {
     ResultMessageScreenContainer(
         message = stringResource(R.string.email_verification_error),
+        iconRes = R.drawable.error_icon,
+        iconDescription = "Error",
+        iconBackgroundGradient = GlanceTheme.errorGradient,
         buttonText = stringResource(R.string.sign_in),
         onContinueButtonClick = onContinueButtonClick
     )
