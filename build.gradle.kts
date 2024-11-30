14e996f5c69e587f16a31d891444b412ccfe56e9
Erwin Elder <erwineldermail@gmail.com>
2024-11-30 13:07:07 +0100

    Change Kotlin version to 2.1.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
-        classpath(kotlin("gradle-plugin", version = "2.0.0"))
+        classpath(kotlin("gradle-plugin", version = "2.1.0"))
     }
 }
 
