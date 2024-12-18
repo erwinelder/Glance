37493cd441233678301671e8ab54ae6deb8f7ff0
Erwin Elder <erwineldermail@gmail.com>
2024-12-18 20:14:34 +0100

    Move app background to a separate file.

--- app/src/main/java/com/ataglance/walletglance/core/presentation/components/GlanceAppComponent.kt
+++ app/src/main/java/com/ataglance/walletglance/core/presentation/components/GlanceAppComponent.kt
@@ -1,13 +1,8 @@
 package com.ataglance.walletglance.core.presentation.components
 
 import androidx.activity.ComponentActivity
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.togetherWith
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
@@ -19,17 +14,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
-import com.ataglance.walletglance.R
 import com.ataglance.walletglance.auth.domain.model.AuthController
 import com.ataglance.walletglance.billing.domain.model.BillingSubscriptionManager
-import com.ataglance.walletglance.core.domain.app.AppTheme
 import com.ataglance.walletglance.core.presentation.GlanceTheme
 import com.ataglance.walletglance.core.presentation.WalletGlanceTheme
+import com.ataglance.walletglance.core.presentation.components.other.AppBackground
 import com.ataglance.walletglance.core.presentation.viewmodel.AppViewModel
 import com.ataglance.walletglance.navigation.presentation.viewmodel.NavigationViewModel
 import com.ataglance.walletglance.personalization.presentation.viewmodel.PersonalizationViewModel
@@ -83,31 +75,5 @@ fun GlanceAppComponent(
                 }
             }
         }
-    }
-}
-
-@Composable
-private fun AppBackground(appTheme: AppTheme?) {
-    AnimatedContent(
-        targetState = appTheme,
-        label = "App background",
-        transitionSpec = {
-            fadeIn() togetherWith fadeOut()
-        }
-    ) { targetAppTheme ->
-        targetAppTheme?.let {
-            val imageAndDescription = when (it) {
-                AppTheme.LightDefault -> R.drawable.main_background_light to
-                        "application light background"
-                AppTheme.DarkDefault -> R.drawable.main_background_dark to
-                        "application dark background"
-            }
-            Image(
-                painter = painterResource(imageAndDescription.first),
-                contentDescription = imageAndDescription.second,
-                contentScale = ContentScale.FillBounds,
-                modifier = Modifier.fillMaxSize()
-            )
-        } ?: Box(modifier = Modifier.fillMaxSize())
     }
 }
---
+++ app/src/main/java/com/ataglance/walletglance/core/presentation/components/other/AppBackground.kt
@@ -0,0 +1,41 @@
+package com.ataglance.walletglance.core.presentation.components.other
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.ataglance.walletglance.R
+import com.ataglance.walletglance.core.domain.app.AppTheme
+
+@Composable
+fun AppBackground(appTheme: AppTheme?) {
+    AnimatedContent(
+        targetState = appTheme,
+        label = "App background",
+        transitionSpec = {
+            fadeIn() togetherWith fadeOut()
+        }
+    ) { targetAppTheme ->
+        targetAppTheme?.let {
+            val imageAndDescription = when (it) {
+                AppTheme.LightDefault -> R.drawable.main_background_light to
+                        "application light background"
+                AppTheme.DarkDefault -> R.drawable.main_background_dark to
+                        "application dark background"
+            }
+            Image(
+                painter = painterResource(imageAndDescription.first),
+                contentDescription = imageAndDescription.second,
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier.fillMaxSize()
+            )
+        } ?: Box(modifier = Modifier.fillMaxSize())
+    }
+}
