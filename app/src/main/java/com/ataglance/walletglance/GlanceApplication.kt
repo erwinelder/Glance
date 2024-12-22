3b932ba92c0625a5bec53337f01c2cd4547bdda7
Erwin Elder <erwineldermail@gmail.com>
2024-12-22 11:33:56 +0100

    Move start koin function to a separate file.

--- app/src/main/java/com/ataglance/walletglance/GlanceApplication.kt
+++ app/src/main/java/com/ataglance/walletglance/GlanceApplication.kt
@@ -1,22 +1,17 @@
 package com.ataglance.walletglance
 
 import android.app.Application
-import com.ataglance.walletglance.di.appModule
-import com.ataglance.walletglance.di.repositoryModule
-import com.ataglance.walletglance.di.viewModelModule
+import com.ataglance.walletglance.di.initKoin
 import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
 
 class GlanceApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        startKoin {
+        initKoin {
             androidContext(this@GlanceApplication)
-            modules(appModule, repositoryModule, viewModelModule)
         }
-
     }
 
 }
