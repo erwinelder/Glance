857dbc7e1ccc1f23f132dc41533cc8174c60553a
Erwin Elder <erwineldermail@gmail.com>
2025-03-09 16:04:00 +0100

    Fix bug related to transfers. Update app version to 4.1.3 (83).

--- app/src/main/java/com/ataglance/walletglance/record/domain/usecase/GetRecordStacksInDateRangeUseCase.kt
+++ app/src/main/java/com/ataglance/walletglance/record/domain/usecase/GetRecordStacksInDateRangeUseCase.kt
@@ -5,6 +5,9 @@ import com.ataglance.walletglance.record.domain.model.RecordStack
 import kotlinx.coroutines.flow.Flow
 
 interface GetRecordStacksInDateRangeUseCase {
+
     fun getFlow(range: LongDateRange): Flow<List<RecordStack>>
+
     suspend fun get(range: LongDateRange): List<RecordStack>
+
 }
