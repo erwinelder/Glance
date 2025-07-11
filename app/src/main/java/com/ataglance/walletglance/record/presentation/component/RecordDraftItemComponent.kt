fc237cb0d2499f69db4015f8bdbf0ac88888aa0f
Erwin Elder <erwineldermail@gmail.com>
2025-07-12 00:35:10 +0200

    Fix keyboard type for Note field at Record Creation screen.

--- app/src/main/java/com/ataglance/walletglance/record/presentation/component/RecordDraftItemComponent.kt
+++ app/src/main/java/com/ataglance/walletglance/record/presentation/component/RecordDraftItemComponent.kt
@@ -218,7 +218,7 @@ private fun RecordItemCreationComponentExpanded(
             labelText = stringResource(R.string.note),
             placeholderText = stringResource(R.string.note_placeholder),
             fontSize = 18.sp,
-            keyboardType = KeyboardType.Number,
+            keyboardType = KeyboardType.Text,
             imeAction = ImeAction.Next
         )
 
